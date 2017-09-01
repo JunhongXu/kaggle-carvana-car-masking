4ecc98463410719d449ab7104372ed07d218b690
Junhong Xu <jxu7@bsu.edu>
2017-09-01 14:43:19 -0400

    more data augmentation methods 😏😏😏😏😏

--- dataset.py
+++ dataset.py
@@ -7,7 +7,7 @@ import cv2
 import torch
 import random
 import time
-
+import math
 
 CARANA_DIR = '/media/jxu7/BACK-UP/Data/carvana'
 mean = [
@@ -150,10 +150,41 @@ class HorizontalFlip(object):
 
 
 class RandomRotate(object):
-    def __call__(self, data):
+    def __call__(self, data, shift_limit=(-0.0625, 0.0625), scale_limit=(1 / 1.1, 1.1),
+                                       rotate_limit=(-45, 45), aspect_limit=(1, 1), borderMode=cv2.BORDER_REFLECT_101,
+                                       u=0.5):
+
+        # cv2.BORDER_REFLECT_101  cv2.BORDER_CONSTANT
         img, l = data
-        if random.random() < 0.5:
-            pass
+        if random.random() < u:
+            height, width, channel = img.shape
+
+            angle = random.uniform(rotate_limit[0], rotate_limit[1])  # degree
+            scale = random.uniform(scale_limit[0], scale_limit[1])
+            aspect = random.uniform(aspect_limit[0], aspect_limit[1])
+            sx = scale * aspect / (aspect ** 0.5)
+            sy = scale / (aspect ** 0.5)
+            dx = round(random.uniform(shift_limit[0], shift_limit[1]) * width)
+            dy = round(random.uniform(shift_limit[0], shift_limit[1]) * height)
+
+            cc = math.cos(angle / 180 * math.pi) * (sx)
+            ss = math.sin(angle / 180 * math.pi) * (sy)
+            rotate_matrix = np.array([[cc, -ss], [ss, cc]])
+
+            box0 = np.array([[0, 0], [width, 0], [width, height], [0, height], ])
+            box1 = box0 - np.array([width / 2, height / 2])
+            box1 = np.dot(box1, rotate_matrix.T) + np.array([width / 2 + dx, height / 2 + dy])
+
+            box0 = box0.astype(np.float32)
+            box1 = box1.astype(np.float32)
+            mat = cv2.getPerspectiveTransform(box0, box1)
+
+            img = cv2.warpPerspective(img, mat, (width, height), flags=cv2.INTER_LINEAR,  borderMode=borderMode,
+                                      borderValue=(0, 0, 0,))
+            l = cv2.warpPerspective(img, mat, (width, height), flags=cv2.INTER_LINEAR, borderMode=borderMode,
+                                    borderValue=(0, 0, 0,))
+
+        return img, l
 
 
 class RandomHueSaturationValue(object):
