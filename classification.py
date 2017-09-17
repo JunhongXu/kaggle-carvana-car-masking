ed309c0194723a809149e28c57c4fd46a139b692
Junhong Xu <jxu7@bsu.edu>
2017-09-17 19:16:27 -0400

    pre-trained on classification

--- classification.py
+++ classification.py
@@ -4,17 +4,27 @@ from torch.autograd import Variable
 from torchvision.models.resnet import resnet34, resnet50, model_zoo, model_urls
 from cls_labels import idx2label, label2idx
 from torch.optim import SGD
-from dataset import CARANA_DIR, get_cls_train_dataloader, get_cls_valid_dataloader
+from dataset import CARANA_DIR, get_cls_train_dataloader, get_cls_valid_dataloader\
+    , get_train_dataloader, get_valid_dataloader, transform3, transform2, transform1
+from refinenet import RefineNetV4_1024, BasicBlock
+from main import SoftIoULoss, BCELoss2d, calculate_weight, lr_scheduler
 import numpy as np
-from util import Logger
+from util import Logger, dice_coeff, pred
 import time
 import random
 import glob
+import cv2
 
 
-MODEL_NAME = 'resnet34_car'
-START_EPOCH = 8
+PRE_TRAIN_MODEL_NAME = 'resnet34_car'
+START_EPOCH = 0
 END_EPOCH = 40
+in_h, in_w, out_h, out_w = 512, 512, 512, 512
+MODEL_NAME = 'pre_trained_refinev4_512'
+BATCH_SIZE = 16
+EVAL_BATCH_SIZE = 32
+print_it = 20
+DEBUG = True
 
 
 def lr_adjuster(optimizer, e):
@@ -79,14 +89,15 @@ def pretrained_resnet34(num_classes=len(label2idx)):
     return model
 
 
-def train():
+def pretrain():
+    start = 15
+    end = 40
     train_loader, valid_loader = get_cls_train_dataloader(), get_cls_valid_dataloader()
-    # net = resnet34(pretrained=True, num_classes=len(label2idx))
     net = pretrained_resnet34()
     net = nn.DataParallel(net).cuda()
     criterion = nn.CrossEntropyLoss()
     optimizer = SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
-    logger = Logger(str(MODEL_NAME))
+    logger = Logger(str(PRE_TRAIN_MODEL_NAME))
     logger.write('-----------------------Network config-------------------\n')
     logger.write(str(net)+'\n')
     total_num = len(train_loader.dataset)
@@ -94,9 +105,9 @@ def train():
     best_val_loss = np.inf
 
     logger.write('EPOCH || CLS loss || Avg CLS loss || Train Acc || Val loss || Val Acc || Time\n')
-    if START_EPOCH > 0:
-        net.load_state_dict(torch.load('models/resnet34_car.pth'))
-    for e in range(START_EPOCH, END_EPOCH):
+    if start > 0:
+        net.load_state_dict(torch.load('models/'+PRE_TRAIN_MODEL_NAME+'.pth'))
+    for e in range(start, end):
         tic = time.time()
         net.train()
         lr_adjuster(optimizer, e)
@@ -127,6 +138,12 @@ def train():
 
         if e % 1 == 0:
             labels, eval_loss, eval_acc = evaluate(net, valid_loader, criterion)
+            if DEBUG:
+                img_names = valid_loader.dataset.img_names
+                for label, img_name in zip(labels, img_names):
+                    cv2.imshow('pic', cv2.imread(img_name))
+                    print(idx2label[label])
+                    cv2.waitKey()
 
             tac = time.time()
             print('\r %s || %.5f || %.5f || %.4f || %.5f || %.4f || %.2f'
@@ -139,11 +156,109 @@ def train():
 
             if eval_loss < best_val_loss:
                 best_val_loss = eval_loss
+                torch.save(net.state_dict(), 'models/' + PRE_TRAIN_MODEL_NAME + '.pth')
+    logger.save()
+    logger.file.close()
+
+
+def load_pretrained_refinenet():
+    pretrain_dict = torch.load('models/{}.pth'.format(PRE_TRAIN_MODEL_NAME))
+    net = nn.DataParallel(RefineNetV4_1024(BasicBlock, [3, 4, 6, 3]))
+    state_dict = net.state_dict()
+    state_dict.update({key: state_dict[key] for key in pretrain_dict if 'fc' not in key})
+    return net
+
+
+def train_seg():
+    train_loader, valid_loader = get_train_dataloader(batch_size=BATCH_SIZE, split='train-4088', mean=None, std=None, H=in_h, W=in_w,
+                                                      out_h=out_h, out_w=out_w), \
+                                 get_valid_dataloader(batch_size=EVAL_BATCH_SIZE, H=in_h, W=in_w, out_h=out_h, out_w=out_w, split='valid-300')
+
+    net = load_pretrained_refinenet().cuda()
+
+    optimizer = SGD([param for param in net.parameters() if param.requires_grad], lr=0.001, momentum=0.9,
+                    weight_decay=0.0005)
+    bce2d = BCELoss2d()
+    # softdice = SoftDiceLoss()
+    softiou = SoftIoULoss()
+    if torch.cuda.is_available():
+        net.cuda()
+    logger = Logger(str(MODEL_NAME))
+    logger.write('-----------------------Network config-------------------\n')
+    logger.write(str(net) + '\n')
+    train_loader.dataset.transforms = transform3
+    if START_EPOCH > 0:
+        net.load_state_dict(torch.load('models/' + MODEL_NAME + '.pth'))
+        logger.write(('------------Resume training %s from %s---------------\n' % (MODEL_NAME, START_EPOCH)))
+
+    # train
+    logger.write('EPOCH || BCE loss || Avg BCE loss || Train Acc || Val loss || Val Acc || Time\n')
+    best_val_loss = 0.0
+    for e in range(START_EPOCH, END_EPOCH):
+        # iterate over batches
+        lr_scheduler(optimizer, e)
+        moving_bce_loss = 0.0
+
+        if e > 10:
+            # reduce augmentation
+            train_loader.dataset.transforms = transform2
+
+        num = 0
+        total = len(train_loader.dataset.img_names)
+        tic = time.time()
+        for idx, (img, label) in enumerate(train_loader):
+            net.train()
+            num += img.size(0)
+            img = Variable(img.cuda()) if torch.cuda.is_available() else Variable(img)
+            # label = label.long()
+            label = Variable(label.cuda()) if torch.cuda.is_available() else Variable(label)
+            out, logits = net(img)
+            # out = out.Long()
+            weight = calculate_weight(label)
+            bce_loss = bce2d(out, label, weight=weight)
+            loss = bce_loss + softiou(out, label)
+            moving_bce_loss += bce_loss.data[0]
+
+            logits = logits.data.cpu().numpy() > 0.5
+            train_acc = dice_coeff(np.squeeze(logits), label.data.cpu().numpy())
+
+            if idx == 0:
+                optimizer.zero_grad()
+            loss.backward()
+            if idx % 2 == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+
+            if idx % print_it == 0:
+                smooth_loss = moving_bce_loss / (idx + 1)
+                tac = time.time()
+                print('\r %.3f || %.5f || %.5f || %.4f || ... || ... || % .2f'
+                      % (num / total, bce_loss.data[0], smooth_loss, train_acc, (tac - tic) / 60),
+                      flush=True, end='')
+
+        if e % 1 == 0:
+            # validate
+            smooth_loss = moving_bce_loss / (idx + 1)
+            pred_labels = pred(valid_loader, net)
+            valid_loss = evaluate(valid_loader, net, bce2d)
+            # print(pred_labels)
+            dice = dice_coeff(preds=pred_labels, targets=valid_loader.dataset.labels)
+            tac = time.time()
+            print('\r %s || %.5f || %.5f || %.4f || %.5f || %.4f || %.2f'
+                  % (e, bce_loss.data[0], smooth_loss, train_acc, valid_loss, dice, (tac - tic) / 60),
+                  flush=True, end='')
+            print('\n')
+            logger.write('%s || %.5f || %.5f || %.4f || %.5f || %.4f || %.2f\n'
+                         % (e, bce_loss.data[0], smooth_loss, train_acc, valid_loss, dice, (tac - tic) / 60), False)
+            logger.log(train_acc, dice, time=(tac - tic) / 60, train_loss=bce_loss.data[0], val_loss=valid_loss)
+            if best_val_loss < dice:
                 torch.save(net.state_dict(), 'models/' + MODEL_NAME + '.pth')
+                best_val_loss = dice
     logger.save()
     logger.file.close()
 
 
 if __name__ == '__main__':
     # train_valid_split()
-    train()
+    # pretrain()
+    train_seg()
